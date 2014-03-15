7daff43712af1ef9a7cb60c0b922ef87eed04eff
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-03-15 22:14:46 +0000

    docking station can release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,12 @@ describe "Docking station" do
     expect(station.bike_count).to eq(1)
   end
 
+  it "should be able to release a bike" do
+    bike = Bike.new
+    station = DockingStation.new
+    station.dock(bike)
+    station.release(bike)
+    expect(station.bike_count).to eq(0)
+  end
+
 end
