618aded1f793534df7ccc7397f5b9784bae9fab4
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-03-15 21:52:13 +0000

    added functionality for fixing the bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,19 @@
 class Bike
 
+  def initialize
+    @broken = false
+  end
+
   def broken?
+    @broken
+  end
+
+  def break
+    @broken = true
+  end
+
+  def fix
+    @broken = false
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,4 +7,18 @@ describe "Bike" do
     expect(bike).not_to be_broken
   end
 
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
+  end
+
+  it "should be able to get fixed" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
+    bike.fix
+    expect(bike).not_to be_broken
+  end
+
 end
