eaeb60edb7ac4a07decec311c611e2db30b8f10d
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-03-15 22:17:41 +0000

    small refaction

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,16 @@ require "./lib/bike"
 
 describe "Docking station" do
 
+  let(:bike)    { Bike.new           }
+  let(:station) { DockingStation.new }
+
   it "should accept a bike" do
-    bike = Bike.new
-    station = DockingStation.new
     expect(station.bike_count).to eq(0)
     station.dock(bike)
     expect(station.bike_count).to eq(1)
   end
 
   it "should be able to release a bike" do
-    bike = Bike.new
-    station = DockingStation.new
     station.dock(bike)
     station.release(bike)
     expect(station.bike_count).to eq(0)
